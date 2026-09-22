9d479f77d5bd110dc1d0196a55327d79bff7bfd7
Joshua Wise <Joshua.Wise722@gmail.com>
2026-09-22 08:10:46 -0400

    26 done

--- easy/26_remove_duplicated_from_sorted_array/main.cpp
+++ easy/26_remove_duplicated_from_sorted_array/main.cpp
@@ -3,7 +3,16 @@
 class Solution {
 public:
   int removeDuplicates(std::vector<int> &nums) {
-    // TODO work on this
-    return 0;
+    if (nums.size() == 0)
+      return 0;
+
+    size_t k = 1;
+    for (size_t i = 1; i < nums.size() - 1; ++i) {
+      if (nums[i] != nums[i - 1]) {
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+    return k;
   }
 };
